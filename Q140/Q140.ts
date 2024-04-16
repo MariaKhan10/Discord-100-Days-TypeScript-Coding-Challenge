614cd72802444e234a12006fad6bc800ca6ec276
Maria Khan <mariapervaiz40@gmail.com>
2024-04-17 04:43:40 +0500

    first commit

---
+++ Q140/Q140.ts
@@ -0,0 +1,8 @@
+// Explain the error that occurs when trying to use a reserved word as a variable name.
+
+
+// let if = 5  // this line would cause a syntax error
+
+console.log("Using a reserved word as a variable name causes a syntax error")
+
+// it is important to avoid using reserved words as identifiers to prevent these kind of errors.
