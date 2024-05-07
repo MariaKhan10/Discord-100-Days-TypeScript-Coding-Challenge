261dfcdf5512bfc885cbe3dd0eecf801accdb25b
Maria Khan <mariapervaiz40@gmail.com>
2024-05-07 22:57:34 +0500

    first commit

--- Q148/Q148.ts
+++ Q148/Q148.ts
@@ -1,4 +1,4 @@
-// Demonstrate  the use of the setTimeout() function to execuute code after delay.
+// Demonstrate  the use of the setTimeout() function to execute code after delay.
 // setTimeout() is a built in javascript function that allows you to execute a piece of code after a specified delay.it is especially useful for scheduling tasks,animations or delaying execution without blocking the main thread.
 
 
